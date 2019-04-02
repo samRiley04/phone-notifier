120a3b6ac81e6070844f82859f576c1416909752
Sam <snmriley@gmail.com>
2019-04-02 16:59:35 +0800

    Updating to normal names. no code changes

---
+++ notif.py
@@ -0,0 +1,13 @@
+import requests
+
+
+
+testDict = {
+	'token':'a5o3won9ek3xzsdbav7xtg3xad8wn7',
+	'user':'uwsp2gr24e877jtjrjc83w5yxvwk6w',
+	'message':'This is a test',
+	'title':'name jeff'
+}
+
+response = requests.post(url = "https://api.pushover.net/1/messages.json", data = testDict)
+print(response.text)
